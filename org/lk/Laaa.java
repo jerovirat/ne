7374950124e2005c2178da9ce1f8d8150d9fa261
jerovirat <jerovirat18@gmail.com>
2018-10-25 20:15:23 +0530

    create a new method

--- org/lk/Laaa.java
+++ org/lk/Laaa.java
@@ -4,7 +4,12 @@ public class Laaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("asdfghj");
 
+	}
+	private void manoj() {
+		// TODO Auto-generated method stub
+System.out.println("zxcvb");
 	}
 
 }
